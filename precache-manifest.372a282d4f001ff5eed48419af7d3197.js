74590184886f57f9e9f6efce76087d76616b81ac
unknown <irinaswofford@gmai.com>
2020-04-14 13:11:02 -0500

    Updates

---
+++ precache-manifest.372a282d4f001ff5eed48419af7d3197.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e202c6e6dd58146f4323f83b5789cb6",
+    "url": "/chart-project/index.html"
+  },
+  {
+    "revision": "8475aeb4f64a5dc7c923",
+    "url": "/chart-project/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "6470f3d4c0e078eb5322",
+    "url": "/chart-project/static/css/main.e68974af.chunk.css"
+  },
+  {
+    "revision": "8475aeb4f64a5dc7c923",
+    "url": "/chart-project/static/js/2.234c9167.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/chart-project/static/js/2.234c9167.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6470f3d4c0e078eb5322",
+    "url": "/chart-project/static/js/main.34084872.chunk.js"
+  },
+  {
+    "revision": "36e71fa1ff286a5d7ebc",
+    "url": "/chart-project/static/js/runtime-main.c51b476f.js"
+  }
+]);
